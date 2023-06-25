3a3d781c0508db4e4daf30c7bf9d214ccb4925be
monomonosu <masanori.ono@arsaga.jp>
2023-06-25 21:11:26 +0900

    ログイン判定追加。ログインしていなければログインページにリダイレクトするようにした。

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -1,14 +1,31 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, redirect } from "react-router-dom";
+import { UseAuthUser } from "./hooks/useAuth";
 import DashboardPage from "./pages/Dashboard";
 import LoginPage from "./pages/Login";
 
+// ログイン済みでアクセス可能なもの
+const guardLoader = async () => {
+  const user = await UseAuthUser();
+  console.log(user);
+  return user.length ? true : redirect("/login");
+};
+
+// ログインしていない時のみアクセス可能
+const guestLoader = async () => {
+  const user = await UseAuthUser();
+  console.log(user);
+  return user.length ? redirect("/") : true;
+};
+
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element:<DashboardPage />
-    },
-    {
-        path: '/login',
-        element:<LoginPage />
-    },
-]) 
+  {
+    path: "/",
+    element: <DashboardPage />,
+    loader: guardLoader,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+    loader: guestLoader,
+  },
+]);
